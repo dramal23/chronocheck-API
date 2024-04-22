09c9745a0fbc1dbb59c96417f9759ff8ca4aa292
dramal23 <alexandre.mrad@dauphine.eu>
2024-04-22 12:49:16 +0200

    added CORS support for browsers compatibility

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = 4000 || process.env.PORT;
 const watchesRouter = require('./routes/watches');
 
 app.get('/', (req, res) => {
   res.json({message: 'alive'});
 });
+
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
 
 app.use('/watches', watchesRouter);
 app.use('/brandnames', watchesRouter);
